0bb9fe16b2b9b6ec18248e6aa2298ac47828e6c7
unibox <unibox@duck.com>
2025-02-15 13:52:25 +0100

    remove output: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wedev-blog.netlify.app',
   integrations: [mdx(), sitemap()],
-  output: 'server',
+
   adapter: netlify(),
 });
