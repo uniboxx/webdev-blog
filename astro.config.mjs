3c47e1e1c7e9b11f78f0e812a0b496dc720b0f41
unibox <unibox@duck.com>
2025-02-15 13:22:46 +0100

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wedev-blog.netlify.app',
   integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
